67b94b2e561c99a22b795d759bbf998c79351670
code-qtzl <azepeda90@outlook.com>
2025-02-15 20:09:10 -0600

    ch3: challenge1 - Billing System

---
+++ ch3/challenge1/README.md
@@ -0,0 +1,12 @@
+## Billing system:
+
+The functions calculate the bill based on:
+
+-   The number of messages sent
+-   The cost per message
+-   Thresholds for discounts
+
+## Results
+
+<img src="img/result1.png" alt="Game Play 1"
+     width="443.66" height="180">
--- ch3/challenge1/main.go
+++ ch3/challenge1/main.go
@@ -7,14 +7,14 @@ func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
 	baseBill := calculateBaseBill(costPerMessage, numMessages)
 	discountRate := calculateDiscountRate(numMessages)
 
-	fmt.Println("Base bill teststs:", baseBill)
+	fmt.Println("Base bill:", baseBill)
 	fmt.Println("discountRate:", discountRate)
 
 	return baseBill * (1 - discountRate)
 }
 
 func calculateDiscountRate(messagesSent int) float64 {
-	if messagesSent >= 5000 {
+	if messagesSent > 5000 {
 		return 0.20
 	} else if messagesSent > 1000 {
 		return 0.10
